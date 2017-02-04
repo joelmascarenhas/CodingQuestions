5b66c2d13aa1599df9d31ec2f6e83596d1e365b2
Joel Mascarenhas <joeljmj26@gmail.com>
2017-02-03 21:46:55 -0700

    CountPairsWithGivenSum

---
+++ InterviewPractice/src/CodeInterviewBookQuestions/CountPairsWithGivenSum.java
@@ -0,0 +1,47 @@
+package CodeInterviewBookQuestions;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * Created by Joel on 03-02-2017.
+ */
+public class CountPairsWithGivenSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Array Size");
+        int no = scanner.nextInt();
+        System.out.println("Enter Elements");
+        int arr[] = new int[no];
+        for(int i=0;i<no;i++)
+        {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Enter Sum");
+        int sum = scanner.nextInt();
+        System.out.println(Countofpairs(arr,sum));
+    }
+    static int Countofpairs(int arr[],int sum)
+    {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int temp,count=0;
+        for(int a:arr)
+        {
+            if(map.containsKey(a))
+                map.put(a,map.get(a)+1);
+            else
+                map.put(a,1);
+        }
+        for(int a:arr)
+        {
+            temp = sum - a;
+            if((temp == a)&&(map.get(a)>1))
+            {
+                count+=map.get(a)-1;
+            }
+            else if(map.containsKey(temp))
+                count+=map.get(temp);
+        }
+        return (count%2==0)?count/2:(count/2)+1;
+    }
+}
