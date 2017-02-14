bccdc1ba3172c0e38d5edf4eff28abe5f7b0faef
Joel Mascarenhas <joeljmj26@gmail.com>
2017-02-13 22:45:52 -0700

    ChocolateDistributionProblem - SolutionPending

---
+++ InterviewPractice/src/HackerRank/ChocolateDistributionProblem.java
@@ -0,0 +1,30 @@
+package HackerRank;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Created by Joel on 12-02-2017.
+ * //Incomplete - work in Progress
+ */
+public class ChocolateDistributionProblem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int testcases = scanner.nextInt();
+        for(int i=0;i<testcases;i++)
+        {
+            System.out.println("No of colleagues");
+            int colcount = scanner.nextInt();
+            int arr[] = new int[colcount];
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
