4cf1ff1761d4c88739bd703b9a6ef91699c98727
Joel Mascarenhas <joeljmj26@gmail.com>
2017-02-23 18:30:03 -0700

    Testing

--- InterviewPractice/src/InterviewQuestions/NoOfIslands.java
+++ InterviewPractice/src/InterviewQuestions/NoOfIslands.java
@@ -6,9 +6,9 @@ package InterviewQuestions;
 public class NoOfIslands {
     public static void main(String[] args) {
         int arr1[][]=  new int[][] {{1, 1, 0, 0, 0},
-                {0, 1, 0, 0, 1},
+                {1, 1, 0, 0, 1},
                 {1, 0, 0, 1, 1},
-                {0, 0, 0, 0, 0},
+                {1, 0, 1, 0, 0},
                 {1, 0, 1, 0, 1}
         };
         int arr[][]=  new int[][] {{0, 0, 0},
